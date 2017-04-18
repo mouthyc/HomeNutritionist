052f92ad492a17cbae4f7f35d71b00c9b49d0fbf
kit ying chan <kit ying chan>
2017-04-18 21:25:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/home/is4032project/NutritionistBlogActivity.java
@@ -0,0 +1,15 @@
+package com.example.home.is4032project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class NutritionistBlogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nutritionistblog);
+    }
+}
